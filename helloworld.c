de365ab4b06c9f31bd28817eaff8d690f65dacf7
schdub <oleg.polivets@gmail.com>
2014-09-13 23:19:47 +0400

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,4 @@
+main(t,O){int _=main;char m[]="+(!#Nvqd=j+jN.7&+0l%0m=%j61N";return
+O==_?((int(*)())O)(_,t+1),68:
+t==_?(*(char*)O?*(char*)O^=((int(*)())t)(O,t):17),O:
+printf(((int(*)())_)(_,m));}
