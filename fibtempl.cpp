3af16a695343c2907358f2ba3aa89053032b7c2d
schdub <oleg.polivets@gmail.com>
2015-04-03 13:45:16 +0300

    Update fibtempl.cpp

--- fibtempl.cpp
+++ fibtempl.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-template <class T, size_t N> void fib(T(&_)[N], size_t i=N) {
-    for(0[_]=0,1[_]=1,i=2;i<N;_[i]=_[i-1]+_[i-2],++i);
+template <class T, size_t N> void fib(T(&a)[N], size_t i=N) {
+  for(0[a]=0,1[a]=1,i=2;i<N;a[i]=a[i-1]+a[i-2],++i);
 }
 int main() {
-    double a[1000];
-    fib(a);
-    for (unsigned i=0; i<sizeof(a)/sizeof(*a); std::cout<<a[i++]<<std::endl);
+  double a[1000];
+  fib(a);
+  for (unsigned i=0; i<sizeof(a)/sizeof(*a); std::cout<<a[i++]<<std::endl);
 }
