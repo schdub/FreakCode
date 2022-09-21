292a825b5ede265668d2ad8ad1563dc8fedc25ba
Oleg V. Polivets <jsbot@ya.ru>
2022-09-21 22:45:02 +0300

    Update stl_magic.cpp

--- stl_magic.cpp
+++ stl_magic.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <vector>
 
 // Напишите алгоритм remove_nth, который удаляет элемент из
 // последовательности по номеру этого элемента.
@@ -13,4 +14,24 @@ FwdIt remove_nth(FwdIt p, FwdIt q, size_t n) {
         *p++ = *next++;
     }
     return p;
+}
+
+// Напишите шаблонную функцию count_permutations, которая принимает
+// некоторую последовательность и вычисляет количество перестановок
+// этой последовательности (равные последовательности считаются одной
+// перестановкой), в которых нет подряд идущих одинаковых элементов.
+
+template<class Iterator>
+size_t count_permutations(Iterator p, Iterator q) {
+    using T = typename std::iterator_traits<Iterator>::value_type;
+    std::vector<T> vec(p, q);
+    std::sort(vec.begin(), vec.end());
+    size_t cnt = 0;
+    do {
+        auto it = std::adjacent_find(vec.begin(), vec.end());
+        if (it == vec.end()) {
+            cnt += 1;
+        }
+    } while (std::next_permutation(vec.begin(), vec.end()));
+    return cnt;
 }
